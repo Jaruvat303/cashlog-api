b399d6496c6e560ae7c1bce2fe12ef06088218fd
Jaruvat303 <jaruvat.see@gmail.com>
2026-07-02 21:20:36 +0700

    refactor(category): change icon url to icon key and add color hex

--- internal/domain/category.go
+++ internal/domain/category.go
@@ -11,23 +11,23 @@ type Category struct {
 	ID        int64     `gorm:"primaryKey;autoIncrement"`
 	Name      string    `gorm:"type:varchar(100);not null;uniqueIndex"`
 	Type      string    `gorm:"type:varchar(100);not null;index"`
-	IconURL   *string   `gorm:"type:varchar(255);column:icon_url"`
+	IconKey   string    `gorm:"type:varchar(50);column:icon_key"`  // เก็บชื่อกลาง เช่น "utensils", "zap", "wallet"
+	ColorHex  string    `gorm:"type:varchar(10);column:color_hex"` // เก็บโค้ดสี เช่น "#EF4444"
 	CreatedAt time.Time `gorm:"autoCreateTime;not null"`
-	UpdateAt  time.Time `gorm:"autoUpdateTime:not null"`
-	Delete    time.Time `gorm:"index"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime:not null"`
 }
 
 type CategoryRepo interface {
 	Create(ctx context.Context, category *Category) error
 	Update(ctx context.Context, updateCat *Category, id uint) error
-	GetAll(ctx context.Context) ([]Category, error)
+	GetByType(ctx context.Context,types string) ([]Category, error)
 	GetByID(ctx context.Context, id uint) (*Category, error)
 	Delete(ctx context.Context, id uint) error
 }
 
 type CategoryUsecase interface {
 	CreateCategory(ctx context.Context, input dto.CreateCategoryInput) error
-	FetchCategories(ctx context.Context) ([]Category, error)
+	FetchCategoriesByType(ctx context.Context, types string) ([]Category, error)
 	UpdateCategory(ctx context.Context, id uint, input dto.UpdateCategoryInput) (*Category, error)
 	DeleteCategory(ctx context.Context, id uint) error
 }
